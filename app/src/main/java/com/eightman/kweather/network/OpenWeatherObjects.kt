0418ad911b65c964955d6891056ac4adec2e0312
Gil Zhaiek <gilzhaiek@gmail.com>
2019-11-01 16:38:11 -0700

    Update OpenWeatherObjects.kt

--- app/src/main/java/com/eightman/kweather/network/OpenWeatherObjects.kt
+++ app/src/main/java/com/eightman/kweather/network/OpenWeatherObjects.kt
@@ -9,8 +9,40 @@ data class City(
     val name: String,
     val country: String,
     val coord: Coord
+) {
+    val inImperial: Boolean get() = country == "US"
+}
+
+data class Weather(
+    val id: Int,
+    val main: String,
+    val description: String,
+    val icon: String
+)
+
+data class Temp(
+    val day: Double,
+    val min: Double,
+    val max: Double,
+    val night: Double,
+    val eve: Double,
+    val morn: Double
+)
+
+data class Forecast(
+    val dt: Int,
+    val sunrise: Int,
+    val sunset: Int,
+    val temp: Temp,
+    val pressure: Int,
+    val humidity: Int,
+    val weather: List<Weather>,
+    val speed: Double,
+    val deg: Int,
+    val clouds: Int
 )
 
 data class ForecastResponse(
-    @Json(name = "city") val city: City
+    @Json(name = "city") val city: City,
+    @Json(name = "list") val forecast: List<Forecast>
 )
