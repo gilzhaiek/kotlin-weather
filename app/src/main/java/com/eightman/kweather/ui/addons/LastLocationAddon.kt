d64c9d383bd46c6bf42e6cba79003fd3bca66b26
Gil Zhaiek <gilzhaiek@gmail.com>
2019-11-03 19:07:39 -0800

    LastLocationAddon

---
+++ app/src/main/java/com/eightman/kweather/ui/addons/LastLocationAddon.kt
@@ -0,0 +1,82 @@
+package com.eightman.kweather.ui.addons
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import com.eightman.kweather.KWeatherApplication.Companion.instance
+import com.google.android.gms.location.*
+
+interface LastLocationAddon : LifecycleObserver {
+    fun getContext(): Context?
+
+    fun onLocationUpdated(location: Location)
+
+    fun requestPermissions(permissions: Array<String>, requestCode: Int) // Fragment class
+
+    fun hasOrRequestLocationPermission(): Boolean? =
+        if (ContextCompat.checkSelfPermission(
+                instance, LOCATION_PERMISSION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestLocationPermission()
+            null
+        } else {
+            true
+        }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun requestLastLocationOrStartLocationUpdates() {
+        hasOrRequestLocationPermission() ?: return
+        val context = getContext() ?: return
+
+        with(LocationServices.getFusedLocationProviderClient(context)) {
+            lastLocation.addOnSuccessListener { location ->
+                when (location) {
+                    null -> startLocationUpdates(this)
+                    else -> onLocationUpdated(location)
+                }
+            }
+        }
+    }
+
+    fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST &&
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            requestLastLocationOrStartLocationUpdates()
+        }
+    }
+
+    private fun startLocationUpdates(fusedLocationClient: FusedLocationProviderClient) {
+        hasOrRequestLocationPermission() ?: return
+
+        fusedLocationClient.requestLocationUpdates(
+            LocationRequest().setPriority(LocationRequest.PRIORITY_LOW_POWER),
+            object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult?) {
+                    locationResult?.lastLocation?.let {
+                        onLocationUpdated(it)
+                    }
+                }
+            },
+            null
+        )
+    }
+
+    private fun requestLocationPermission() {
+        requestPermissions(arrayOf(LOCATION_PERMISSION), LOCATION_PERMISSION_REQUEST)
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST = 1
+        private const val LOCATION_PERMISSION = "android.permission.ACCESS_FINE_LOCATION"
+    }
+}
--- app/src/main/java/com/eightman/kweather/ui/fragments/ForecastFragment.kt
+++ app/src/main/java/com/eightman/kweather/ui/fragments/ForecastFragment.kt
@@ -1,5 +1,6 @@
 package com.eightman.kweather.ui.fragments
 
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,28 +8,47 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.eightman.kweather.R
 import com.eightman.kweather.network.OpenWeatherApi
+import com.eightman.kweather.ui.addons.LastLocationAddon
 import kotlinx.android.synthetic.main.fragment_forecast.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class ForecastFragment : Fragment() {
-
+class ForecastFragment : Fragment(), LastLocationAddon {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_forecast, container, false)
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_forecast, container, false)
 
-    override fun onResume() {
-        super.onResume()
+        lifecycle.addObserver(this)
+
+        return view
+    }
+
+    override fun onLocationUpdated(location: Location) {
         GlobalScope.launch(Dispatchers.Main) {
             val forecastResponse = withContext(Dispatchers.IO) {
-                OpenWeatherApi.forecastService.forecast(lat = 49.2827, lon = -123.1207)
+                OpenWeatherApi.forecastService.forecast(
+                    lat = location.latitude,
+                    lon = location.longitude
+                )
             }
 
             cityName.text = forecastResponse.city.name
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super<Fragment>.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        super<LastLocationAddon>.onRequestPermissionsResult(
+            requestCode, permissions, grantResults
+        )
     }
 }
